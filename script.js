de3bec740cf1022c72917dfa92a8d34a3cd832d8
Rachel Rawlings <rachel.0827@gmail.com>
2018-08-09 20:02:58 -0700

    step three with error

--- script.js
+++ script.js
@@ -16,14 +16,23 @@ let gators = [
     imgSource: "http://www.vaguebuttrue.com/images/1449775760-alligatorclimbsfenceWEBSITE.jpg"
   }
 ]
-let profileListings
+
+Vue.component(‘headline’, {
+ props: [‘title’],
+ template: ‘<h1><img src=“alligator-logo.png” alt=“Alligator Logo”>{{title}}</h1>’
+})
 
 document.addEventListener("DOMContentLoaded", function(){
+
 profileListings = new Vue({
   el: '#profileListings',
   data: {
     profiles: gators
   }
+})
+
+let headerApp = new Vue({
+  el: '#headerApp'
 })
 
 })
